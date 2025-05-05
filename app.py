6acffcdaa80035925847c4bf6a4e2c88c4169166
lampv <lampv01203@gmail.com>
2025-05-05 23:34:29 +0700

    a

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ from flask import Flask, request, jsonify
 import threading
 import os
 
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)  # Cho phép mọi origin
+
 
 COOKIES_URL = "https://r2.lam.io.vn/cookies/x_cookies.txt"
 OUTPUT_FILE = "x_media.json"
